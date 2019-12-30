06c4f7173a854f7cec598a55f33ca814793f60c8
PedroCapa <pmccapa@hotmail.com>
2019-12-30 22:33:40 +0000

    Fiz a parte do Login/Registar

--- Trabalho/server.erl
+++ Trabalho/server.erl
@@ -1,8 +1,10 @@
 -module(server).
 -export([server/1]).
 
+%Verificar novamente as partes de comunicação, por exemplo quando se recebe ou envia alguma coisa
+
 server(Port) ->
-		Room = spawn(fun()-> room([]) end),
+		Room = spawn(fun()-> room(#{}, #{}) end),
 		{ok, LSock} = gen_tcp:listen(Port, [binary, {packet, line}, {reuseaddr, true}]),
 		acceptor(LSock, Room).
 
@@ -12,31 +14,53 @@ acceptor(LSock, Room) ->
 		Room ! {enter, self()},
 		user(Sock, Room).
 
-room(Users) ->
+room(Importadores, Fabricantes) ->
 		receive
 			{enter, Pid} ->
-				io:format("userentered ~w~n", [Pid]),
-				gen_tcp:send(Pid, "You entered in the server"),
-				room(Users);
+				io:format("userentered ~p~n", [Pid]),
+				room(Importadores, Fabricantes);
 			{aut, {Username, Password}, PID}  ->
-				CheckUsername = containsUsername(Username, Users),
-				CheckPassword = containsPassword(Username, Users, Password),
+				Fab = checkUsername(Username, Password, Fabricantes),
+				Imp = checkUsername(Username, Password, Importadores),
+				io:format("~p   ~p ~n", [Fab, Imp]),
 				if
-					CheckUsername =:= true, CheckPassword =:= true -> 
-						PID ! {sucess, {Username, Password}},
-						room(Users);
-					CheckUsername =:= true, CheckPassword =:= false -> 
+					%Mudar para o caso de ser Fabricante ou Importador
+					Imp =:= true ->
+						io:format("O utilizador acertou as credenciais"),
+						PID ! {imp, {Username, Password}},
+						room(Importadores, Fabricantes);
+					Fab =:= true ->
+						io:format("O user acertou"),
+						PID ! {fab, {Username, Password}},
+						room(Importadores, Fabricantes);
+					Fab =:= false, Imp =:= false ->
+						%Enviar a dizer que precisa de indicar se é Importador ou Fabricante
+						io:format("O utilizador registou-se"),
+						PID ! {type, {Username, Password}},
+						room(Importadores, Fabricantes);
+					true ->
+						io:format("Acertou miseravel"),
 						PID ! {tcp, "", ""},
-						room(Users);
-					CheckUsername =:= false -> 
-						PID ! {sucess, {Username, Password}}, 
-						room([{Username, Password} | Users]) 
+						room(Importadores, Fabricantes)
+				end;
+			%Adicionar outro pq ele colocou a dizer se era Fabricante ou Importador
+			{type, {Username, Password}, Type, PID} ->
+				T = checkType(Type),
+				if 
+					T =:= true->
+						PID ! {imp, {Username, Password}},
+						io:format("Username: ~p ~n Password: ~p ~n", [Username, Password]),
+						room(maps:merge(#{Username => Password}, Importadores), Fabricantes);
+					true ->
+						PID ! {fab, {Username, Password}},
+						io:format("Username: ~p ~n Password: ~p ~n", [Username, Password]),
+						room(Importadores, maps:merge(#{Username => Password}, Fabricantes))
 				end;
 			{leave} ->
 				io:format("userleft ~n", []),
-				room(Users);
+				room(Importadores, Fabricantes);
 				_ -> 
-				io:format("Sai da sala ~n", [])
+				io:format("Sai ~n", [])
 		end.
 
 user(Sock, Room) ->
@@ -45,23 +69,35 @@ user(Sock, Room) ->
 			gen_tcp:send(Sock, Data),
 			user(Sock, Room);
 		{tcp, _, _} ->
-			gen_tcp:send(Sock, "Put the credentials"),
+			gen_tcp:send(Sock, "Put the Credentials"),
 			Person = authentication(),
 			Room ! {aut, Person, self()},
 			user(Sock, Room);
-		{sucess, Person} -> 
-			gen_tcp:send(Sock, "authenticated with sucess"),
-			authenticated(Sock, Room, Person);
+		{type, {Username, Password}} ->
+			gen_tcp:send(Sock, "Put the type 0 for Importadores or something else to Fabricantes"),
+			receive
+			{tcp, _, T} ->
+				Type = binary_to_list(T)
+			end,
+			Room ! {type, {Username, Password}, Type, self()},
+			user(Sock, Room);
+		{imp, Person} -> 
+			gen_tcp:send(Sock, "authenticated with sucess Importador"),
+			importador(Sock, Room, Person);
+		{fab, Person} -> 
+			gen_tcp:send(Sock, "authenticated with sucess Fabricante"),
+			fabricante(Sock, Room, Person);
 		{tcp_closed, _} ->
 			Room ! {leave};
 		{tcp_error, _, _} ->
 			Room ! {leave}
 	end.
 
+%Nesta parte o utilizador coloca as credenciais
 authentication() ->
 		receive
-			{tcp, _, Data} ->
-				Username = binary_to_list(Data)
+			{tcp, _, User} ->
+				Username = binary_to_list(User)
 		end,
 		receive
 			{tcp, _, Pass} ->
@@ -69,32 +105,61 @@ authentication() ->
 				{Username, Password}
 		end.
 
-authenticated(Sock, Room, {Username, Password}) ->
+%Depois de serem verificadas as credenciais o utilizador esta nesta parte
+importador(Sock, Room, {Username, Password}) ->
 		receive
 			{line, Data} ->
 				gen_tcp:send(Sock, binary_to_list(Data)),
-				authenticated(Sock, Room, {Username, Password});
+				importador(Sock, Room, {Username, Password});
 			{tcp, _, Data} ->
-				Room ! {line, Data, self(), Username},
-				authenticated(Sock, Room, {Username, Password});
+				io:format("Recebi ~p~n", [binary_to_list(Data)]),
+				importador(Sock, Room, {Username, Password});
 			{tcp_closed, _} ->
 				Room ! {leave, self()};
 			{tcp_error, _, _} ->
 				Room ! {leave, self()}
 		end.
 
-containsUsername(_, []) -> false;
-containsUsername(Username, [{Name, _} | Tail]) ->
-		if Username == Name -> true;
-			true -> containsUsername(Username, Tail)
+fabricante(Sock, Room, {Username, Password}) ->
+		receive
+			{line, Data} ->
+				gen_tcp:send(Sock, binary_to_list(Data)),
+				fabricante(Sock, Room, {Username, Password});
+			{tcp, _, Data} ->
+				io:format("Recebi ~p~n", [binary_to_list(Data)]),
+				fabricante(Sock, Room, {Username, Password});
+			{tcp_closed, _} ->
+				Room ! {leave, self()};
+			{tcp_error, _, _} ->
+				Room ! {leave, self()}
 		end.
 
-containsPassword(_, [], _) -> false;
-containsPassword(Username, [{Name, Pass} | Tail], Password) ->
-		if Username == Name,  Pass == Password ->
-					true;
-				Username == Name,  Pass =:= Password ->
-					false;
-				true ->
-					containsUsername(Username, Tail)
-		end.
+checkUsername(Username, Password, Map) ->
+	User = maps:is_key(Username, Map),
+	io:format("O User e: ~p ~n", [Username]),
+	if
+		User =:= true ->
+			Res = checkPassword(Username, Password, Map),
+			Res;
+		User =/= true ->
+			false
+	end.
+
+checkPassword(Username, Password, Map)  ->
+	{ok, P} = maps:find(Username, Map),
+	io:format("A pass e: ~p ~n", [P]),
+	if
+		P =:= Password ->
+			true;
+		true ->
+			untrue
+	end.
+
+checkType(Type) ->
+	io:format("Type: ~p ~n", [Type]),
+	if
+		Type =:= "0\n" ->
+			true;
+		true ->
+			false
+	end.
