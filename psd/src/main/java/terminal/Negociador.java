8178497b3551cacd75c487a809a221789028cd0c
PedroCapa <pmccapa@hotmail.com>
2020-01-19 10:59:46 +0000

    Fiz os métodos do dropwizard falta enviar a resposta

--- psd/src/main/java/terminal/Negociador.java
+++ psd/src/main/java/terminal/Negociador.java
@@ -4,49 +4,179 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
 
 import main.proto.Protos.ResponseProdutoDropwizard;
 import main.proto.Protos.Production;
 import main.proto.Protos.ResponseNegotiationDropwizard;
 import main.proto.Protos.NegotiationDropwizard;
+import main.proto.Protos.NegSyn;
+import main.proto.Protos.ConfirmNegotiations;
+import main.proto.Protos.AceptedNegotiation;
+import main.proto.Protos.Notification;
+import main.proto.Protos.Negotiation;
+import main.proto.Protos.ProdutoNegociador;
+import main.proto.Protos.OfertaImp;
+import main.proto.Protos.OfertaNegociador;
+import main.proto.Protos.BusinessConfirmation;
+import main.proto.Protos.Dropwizard;
+
+import main.java.terminal.ReadMessage;
+import main.java.terminal.SendMessage;
 
 public class Negociador{
 
 	public static void main(String[] args) {
 		int port = Integer.parseInt(args[0]);
 
-		System system = new System();
+		try{
+			ServerSocket ss = new ServerSocket(port);
+			Sistema system = new Sistema();
+		
+			while(true){
+				try{
+					Socket cs = ss.accept();
+					Leitor lc = new Leitor(system,cs);
+					Thread t = new Thread(lc);
+					t.start();
+				}catch(IOException e){
+					System.out.println(e.getMessage());
+				}
+			}
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+
 	}
 }
 
-class System{
+class Leitor implements Runnable{
+	private Sistema sys;
+	private	ReadMessage rm = new ReadMessage(this.cs);
+	private SendMessage sm = new SendMessage(this.cs);
+	private Socket cs;
+
+	public Leitor(Sistema system, Socket sock){
+		this.sys = system;
+		this.cs  = sock;
+	}
+
+	public void run(){
+		boolean flag = true;
+
+		try{
+			while(flag){
+				byte[] syn = this.rm.receiveMessage();
+				NegSyn negsyn = NegSyn.parseFrom(syn);
+
+				receiveCont(negsyn);
+			}
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void receiveCont(NegSyn negsyn){
+		try{
+
+			byte[] cont = this.rm.receiveMessage();
+			//ProdutoNegociador -> BusinessConfirmation
+			if(negsyn.getType().equals(NegSyn.OpType.FAB_PROD)){
+				ProdutoNegociador pn = ProdutoNegociador.parseFrom(cont);
+				boolean res = sys.addProduct(pn);
+				BusinessConfirmation bc = BusinessConfirmation.newBuilder().
+																setResponse(res).
+																setFabricante(pn.getUsername()).
+																setProduto(pn.getProduct().getName()).
+																build();
+				byte[] bytes = bc.toByteArray();
+				this.sm.sendServer(bytes);
+			}
+			//Notification -> ConfirmNegotiations
+			if(negsyn.getType().equals(NegSyn.OpType.FAB_OVER)){
+				Notification notification = Notification.parseFrom(cont);
+				ConfirmNegotiations cn = sys.getAceptedOffers(notification.getFabricante(), notification.getProduto());
+				byte[] bytes = cn.toByteArray();
+				this.sm.sendServer(bytes);
+			}
+			//OfertaImp -> BusinessConfirmation
+			if(negsyn.getType().equals(NegSyn.OpType.IMP_OFFER)){
+				OfertaNegociador neg = OfertaNegociador.parseFrom(cont);
+				Oferta oferta = new Oferta(neg.getProducer(), neg.getOffer().getImportador(), neg.getProduct(), neg.getOffer().getQuantity(), 
+									neg.getOffer().getPrice(),neg.getOffer().getDate(), neg.getOffer().getState());
+				boolean res = sys.addOffer(oferta);
+				BusinessConfirmation bc = BusinessConfirmation.newBuilder().
+																setResponse(res).
+																setFabricante(neg.getProducer()).
+																setProduto(neg.getProduct()).
+																build();
+				byte[] bytes = bc.toByteArray();
+				this.sm.sendServer(bytes);
+			}
+
+			if(negsyn.getType().equals(NegSyn.OpType.IMP_OVER)){
+				Notification notification = Notification.parseFrom(cont);
+				ConfirmNegotiations cn = sys.getPublishedOffers(notification.getFabricante(), notification.getProduto(), notification.getUsername());
+				byte[] bytes = cn.toByteArray();
+				this.sm.sendServer(bytes);
+			}
+			//Depende do DROP a seguir
+			if(negsyn.getType().equals(NegSyn.OpType.DROP)){
+				Dropwizard drop = Dropwizard.parseFrom(cont);
+				handleDropwizard(drop);
+			}
+
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void handleDropwizard(Dropwizard drop){
+		if(drop.getType().equals(Dropwizard.DropType.PROD)){
+			ResponseProdutoDropwizard rpd = sys.getProdutosUser(drop.getUsername());
+		}
+
+		if(drop.getType().equals(Dropwizard.DropType.NEG)){
+
+		}
+
+		if(drop.getType().equals(Dropwizard.DropType.IMP)){
+			ResponseNegotiationDropwizard rpd = sys.getNegociosProdutoUser(drop.getUsername(), drop.getProd());
+		}
+
+
+	}
+}
+
+class Sistema{
 
 	private Map<String, Produtor> producers;
 
-	public System(){
+	public Sistema(){
 		this.producers = new HashMap<>();
 	}
 
-	public boolean addProdutor(Produtor producer){
-		if(!this.producers.containsKey(producer.getUsername())){
-			this.producers.put(producer.getUsername(), producer);
-			return true;
+	public boolean addProduct(ProdutoNegociador product){
+		if(!this.producers.containsKey(product.getUsername())){
+			this.producers.put(product.getUsername(), new Produtor(product.getUsername()));
 		}
-		return false;
-	}
-	//***Fazer alguma verificação dentro do produto
-	public boolean addProduct(Produto product){
-		String producer = product.getProducer();
-		String product_name = product.getName();
+		String producer = product.getUsername();
+		String product_name = product.getProduct().getName();
 		boolean flag = containsProduct(producer, product_name);
 		if(!flag){
 			Map<String, Produto> products = producers.get(producer).getProducts();
-			products.put(product_name, product);
+			Produto p = new Produto(product.getUsername(), product.getProduct().getName(), product.getProduct().getMin(), product.getProduct().getMax(),
+						product.getProduct().getPrice(), -1, product.getProduct().getDate());
+			products.put(product_name, p);
 		}
 		return false;
 	}
 
-	public void addOffer(Oferta offer){
+	public boolean addOffer(Oferta offer){
 		String producer = offer.getProducer();
 		String product  = offer.getProduct();
 		boolean flag = containsProduct(producer, product);
@@ -54,8 +184,10 @@ class System{
 			Produto prod = this.producers.get(producer).getProducts().get(product);
 			if(prod.getMax() >= offer.getQuantity() && prod.getPrice() <= offer.getPrice() && prod.getDate().compareTo(offer.getDate()) > 0){
 				sortOffers(prod, offer);
+				return true;
 			}
 		}
+		return false;
 	}
 
 	public void sortOffers(Produto prod, Oferta offer){
@@ -84,7 +216,7 @@ class System{
 		return false;
 	}
 
-	public List<Oferta> getAceptedOffers(String producer, String product_name){
+	public ConfirmNegotiations getAceptedOffers(String producer, String product_name){
 		Produto prod = this.producers.get(producer).getProducts().get(product_name);
 		if(prod.getState() == -1){
 			List<Oferta> ofertas = prod.getOffersList();
@@ -102,10 +234,25 @@ class System{
 				accepted.add(of);
 			}
 		}
-		return accepted;
+		List<AceptedNegotiation> an = new ArrayList<>();
+		for(Oferta of: accepted){
+			AceptedNegotiation acn = AceptedNegotiation.newBuilder().
+												setImporterOffer(of.getImportador()).
+												setProductName(of.getProducer()).
+												setPrice(of.getPrice()).
+												setAmount(of.getQuantity()).
+												setData(of.getDate()).
+												setState(of.getState()).
+												build();
+			an.add(acn);
+		}
+		ConfirmNegotiations cn = ConfirmNegotiations.newBuilder().
+												addAllAcepted(an).
+												build();
+		return cn;
 	}
 
-	public List<Oferta> getPublishedOffers(String producer, String product_name, String importador){
+	public ConfirmNegotiations getPublishedOffers(String producer, String product_name, String importador){
 		Produto prod = this.producers.get(producer).getProducts().get(product_name);
 		if(prod.getState() == -1){
 			List<Oferta> ofertas = prod.getOffersList();
@@ -123,7 +270,22 @@ class System{
 				accepted.add(of);
 			}
 		}
-		return accepted;
+		List<AceptedNegotiation> an = new ArrayList<>();
+		for(Oferta of: accepted){
+			AceptedNegotiation acn = AceptedNegotiation.newBuilder().
+												setImporterOffer(of.getImportador()).
+												setProductName(of.getProducer()).
+												setPrice(of.getPrice()).
+												setAmount(of.getQuantity()).
+												setData(of.getDate()).
+												setState(of.getState()).
+												build();
+			an.add(acn);
+		}
+		ConfirmNegotiations cn = ConfirmNegotiations.newBuilder().
+												addAllAcepted(an).
+												build();
+		return cn;
 	}
 
 	public ResponseProdutoDropwizard getProdutosUser(String username){
@@ -189,6 +351,16 @@ class Oferta{
 	private String date;
 	private int state;
 
+	public Oferta(String producer, String importador, String prodduct, int quantity, int price, String date, int state){
+		this.producer = producer;
+		this.importador = importador;
+		this.product = product;
+		this.quantity = quantity;
+		this.price = price;
+		this.date = date;
+		this.state = state;
+	}
+
 	public String getProducer(){
 		return this.producer;
 	}
@@ -232,6 +404,17 @@ class Produto{
 	private int state;
 	private String date;
 	List<Oferta> offers;
+
+	public Produto(String name, String producer, int min, int max, int price, int state, String date){
+		this.name = name;
+		this.producer = producer;
+		this.min = min;
+		this.max = max;
+		this.price = price;
+		this.state = state;
+		this.date = date;
+		this.offers = new ArrayList<>();
+	}
 
 	public String getName(){
 		return this.name;
@@ -302,6 +485,11 @@ class Produtor{
 
 	public String getUsername(){
 		return this.username;
+	}
+
+	public Produtor(String username){
+		this.username = username;
+		this.products = new HashMap<>();
 	}
 
 	public Map<String, Produto> getProducts(){
